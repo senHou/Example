3cf39eb944656a4b02cbec64d9d16928573c0cc3
senhou <hsyy_1987@hotmail.com>
2012-10-05 17:48:26 +1000

    add new file

---
+++ Job/CreditAccount.h
@@ -0,0 +1,13 @@
+//
+//  CreditAccount.h
+//  Job
+//
+//  Created by sen hou on 5/10/12.
+//  Copyright (c) 2012 sen hou. All rights reserved.
+//
+
+#import "BankAccount.h"
+
+@interface CreditAccount : BankAccount
+
+@end
---
+++ Job/CreditAccount.m
@@ -0,0 +1,13 @@
+//
+//  CreditAccount.m
+//  Job
+//
+//  Created by sen hou on 5/10/12.
+//  Copyright (c) 2012 sen hou. All rights reserved.
+//
+
+#import "CreditAccount.h"
+
+@implementation CreditAccount
+
+@end
